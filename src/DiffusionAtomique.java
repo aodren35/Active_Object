9b0231a11724f6ec1f8ab39615f4736975621dcf
CamilleBarbe <camillebarbe@laposte.net>
2017-10-16 17:25:42 +0200

    strategy

---
+++ src/DiffusionAtomique.java
@@ -0,0 +1,12 @@
+public class DiffusionAtomique implements AlgoDiffusion {
+
+    @Override
+    public void configure(GeneratorImpl generatorImpl) {
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
